72481564b84d79ced698e50125298c6b66719e1b
Trevor Masinelli <trevor.m@thezenorange.com>
2017-03-24 20:46:18 -0500

    in-class work from last night

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,15 +1,17 @@
 
 
-var model = {
-  watchlistItems: [],
-  browseItems: []
-}
+var model =
+    {
+        watchlistItems: [],
+        browseItems: []
+    }
 
 
-var api = {
-  root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
-}
+var api =
+    {
+        root: "https://api.themoviedb.org/3",
+        token: "29b8dc890b2a89171bcb484d1f1f77e1"
+    }
 
 
 /**
@@ -17,55 +19,63 @@ var api = {
  * if successful, updates the model.browseItems appropriately, and then invokes
  * the callback function that was passed in
  */
-function discoverMovies(callback) {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-			
-			// TODO 2
-			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
-			// invoke the callback function that was passed in. 
-			callback();
-		}
-	});
-  
-}
+function discoverMovies(callback)
+    {
+    	$.ajax(
+            {
+    		    url: api.root + "/discover/movie",
+    		    data:
+                    {
+    			        api_key: api.token,
+    		        },
+    		    success: function(response)
+                    {
+                        model.browseItems = response.results;
+
+    			        // invoke the callback function that was passed in.
+    			        callback();
+    		        }
+	       });
+    }
 
 
 /**
  * re-renders the page with new content, based on the current state of the model
  */
-function render() {
-  // TODO 7
-  // clear everything from both lists
-  
-  // TODO 6
-  // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
-  // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
-		// TODO 3
-		// insert a list item into the <ul> in the browse section
-		
-		// TODO 4
-		// the list item should include a button that says "Add to Watchlist"
-		
-		// TODO 5
-		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
-  });
-  
-}
+function render()
+    {
+        // flush everything for new data
+        $('#section-watchlist ul').empty();
+        $('#section-browse ul').empty();
+
+        // set up our watch list section
+        model.watchlistItems.forEach(function(movie)
+            {
+                var watchTitle = $("<h5 style='margin: 10px 0 10px 0;'></h5>").text(movie.original_title);
+                var watchThis = $("<li style='list-style: none;'></li>").append(watchTitle);
+                $("#section-watchlist ul").append(watchThis);
+            });
+
+        // for each movie on the current browse list
+        model.browseItems.forEach(function(movie)
+            {
+                var thisTitle = $("<h5 style='margin: 20px 0 3px 0;'></h5>").text(movie.original_title);
+                var movieTitle = $("<li style='list-style: none;'></li>").append(thisTitle);
+                $("#section-browse ul").append(movieTitle);
+
+                // set up the button for each movie, and push that data to the watchList when clicked
+                var thisButton = $("<button></button>").text("Add to Watchlist").click(function()
+                    {
+			            model.watchlistItems.push(movie);
+			            render();
+		            });
+		        movieTitle.append(thisButton);
+            });
+    }
 
 
-// When the HTML document is ready, we call the discoverMovies function,
-// and pass the render function as its callback
-$(document).ready(function() {
-  discoverMovies(render);
-});
-
+// When the HTML document is ready, we call the discoverMovies function, and pass the render function as its callback
+$(document).ready(function()
+    {
+        discoverMovies(render);
+    });
